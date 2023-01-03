4a44ae63e54f20683277b7674cce0cbcbc383f4c
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2023-01-02 19:57:28 -0500

    update dictionary

--- 08_dictionary.py
+++ 08_dictionary.py
@@ -1,9 +1,47 @@
 """
-A dictionary is an unordered set of key: value pairs.
+dictionaries are used to store data values in key:value pairs.
+
+a dictionary is a collection which is ordered*, changeable and do not allow duplicates
+
+as of python version 3.7, dictionaries are ordered. in python 3.6 and earlier, dictionaries 
+are unordered.
 """
-menu = {"avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
-students_in_classes = {"software design": ["Aaron", "Delila", "Samson"], "cartography": ["Christopher", "Juan", "Marco"], "philosophy": ["Frederica", "Manuel"]}
+
+
+my_dictionary = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(my_dictionary)
+
+
+menu = {"Foo": 1, "Bar": 2, "Baz": 2}
+
+
+students_in_classes = {
+        "software design": ["Aaron", "Delila", "Samson"],
+        "history": ["Christopher", "Juan", "Marco"],
+        "philosophy": ["Frederica", "Manuel"]
+        }
+print(students_in_classes)
 
 
 sensors = {}
 sensors.update({"pantry": 22, "guest room": 25, "patio": 34})
+print(sensors)
+
+
+print(sensors.get("pantry"))
+
+sensors.pop("pantry")
+print(sensors)
+
+print(sensors.keys())
+
+print(sensors.values())
+
+print(sensors.items())
+
+sensors.clear()
+print(sensors)
