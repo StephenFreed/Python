f04d3574fa77d9d7696da6203072f84b726f3116
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2023-01-02 19:57:43 -0500

    update functions

--- 09_functions.py
+++ 09_functions.py
@@ -1,21 +1,54 @@
 # Parameters & Arguments
+# Function Signature
 
-def HelloWorld(name):
+def hello_world(name):
     """
-    explain what this function does
+    this function adds argument name to hello world
     """
     print(f"Hello Word From {name}")
 
-HelloWorld("Bob")
 
-
-# keyword Arguments vs positional
-# calculate_taxi_price(rate=0.5, discount=10, miles_to_travel=100)
-
-# Here we are using a total of two built-in functions in our example: print(), and len()
-help("string")
-# https://docs.python.org/3/library/functions.html
-
-# Here we are using a default value for our parameter of `destination` 
 def trip_welcome(destination="California"):
-    print(" Looks like you're going to ")
+    """
+    default value for our parameter of "destination" 
+    """
+    print(f"Looks like you're going to {destination}")
+
+
+hello_world("Foo")
+trip_welcome("Florida")
+trip_welcome()
+
+
+def unknown_arguments_function(*names):
+    """
+    *args
+
+    if you do not know how many arguments that will be passed into your function
+    add a * before the parameter name in the function definition.
+
+    this way the function will receive a tuple of arguments, and can access the items accordingly
+    """
+    name_list = []
+    for name in names:
+        name_list.append(name)
+
+    print(name_list)
+
+unknown_arguments_function("Foo", "Bar", "Baz")
+
+
+def unknown_key_value_function(**names):
+    """
+    **kwargs
+
+    if you do not know how many keyword arguments that will be passed into your function
+    add two asterisk ** before the parameter name in the function definition.
+
+    this way the function will receive a dictionary of arguments, and can access 
+    the items accordingly
+    """
+    for key, value in names.items():
+        print(f"{key}: {value}")
+
+unknown_key_value_function(foo = "bar", bar = "foo", baz = "bar")
